e3a7301ff5c7fa9dbc4d33c9ebc2913e25caf68c
Lucas <lucasalbergaria@gmail.com>
2023-06-21 11:36:05 +0000

    first implementation prefect

--- etl/web_to_gcs.py
+++ etl/web_to_gcs.py
@@ -1,66 +1,59 @@
-import io
-import os
-import requests
+import gzip
 import pandas as pd
-from google.cloud import storage
+from pathlib import Path
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
 
-"""
-Pre-reqs: 
-1. `pip install pandas pyarrow google-cloud-storage`
-2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
-3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
-"""
+@task(retries=3)
+def fetch_data(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+    df = pd.read_csv(dataset_url)
+    return df
 
-# services = ['fhv','green','yellow']
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
-# switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
+@task(log_prints=True)
+def clean_data(df: pd.DataFrame, color: str = 'yellow') -> pd.DataFrame:
+    """Fix dtype issues"""
+    if color == 'yellow':
+        df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+        df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    if color == 'green':
+        df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+        df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    return df
 
+@task()
+def write_local_parquet(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
 
-def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    """
-    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+@task()
+def upload_to_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_bucket = GcsBucket.load("etl-gcp-prefect")
+    gcs_bucket.upload_from_path(from_path=path, to_path=path)
+    return
 
-    client = storage.Client()
-    bucket = client.bucket(bucket)
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
+@flow()
+def etl_web_to_gcs(color: str, year: int, month: int) -> None:
+    """The main ETL function"""
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+    df = fetch_data(dataset_url)
+    df_cleaned = clean_data(df, color)
+    path = write_local_parquet(df_cleaned, color, dataset_file)
+    upload_to_gcs(path)
 
-
-def web_to_gcs(year, service):
-    for i in range(12):
-        
-        # sets the month part of the file_name string
-        month = '0'+str(i+1)
-        month = month[-2:]
-
-        # csv file_name
-        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
-
-        # download it using requests via a pandas df
-        request_url = f"{init_url}{service}/{file_name}"
-        r = requests.get(request_url)
-        open(file_name, 'wb').write(r.content)
-        print(f"Local: {file_name}")
-
-        # read it back into a parquet file
-        df = pd.read_csv(file_name, compression='gzip')
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
-        print(f"Parquet: {file_name}")
-
-        # upload it to gcs 
-        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
-        print(f"GCS: {service}/{file_name}")
-
-
-web_to_gcs('2019', 'green')
-web_to_gcs('2020', 'green')
-# web_to_gcs('2019', 'yellow')
-# web_to_gcs('2020', 'yellow')
+if __name__ == "__main__":
+    colors = ["green"]
+    years = [2019,2020,2021]
+    months = range(1, 13)
+    for color in colors: 
+        for year in years:
+            for month in months:
+                try:
+                    etl_web_to_gcs(color, year, month)
+                except Exception as e:
+                    print(e)
 
